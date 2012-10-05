4a1e1667b5a47fa9746eeabb0ecf0819371a1a79
sv01a <sv01a@live.com>
2012-10-05 14:28:14 +0600

    Update knockout.d.ts

--- knockout.d.ts
+++ knockout.d.ts
@@ -1,10 +1,10 @@
 module knockout {
     interface koSubscription{
         dispose();
-    };
+    }
     interface koExtendOptions {
         throttle?: number;
-    };
+    }
     interface koExtend {
         ();
         extend(options:koExtendOptions);
@@ -13,8 +13,8 @@ module knockout {
         read?: () =>any;
         write?: () =>void;
         owner?: any;
-    };
-    export interface koComputed {
+    }
+    interface koComputed {
         (evaluator: () => any): koExtend;
         (options: koComputedOptions): koExtend;
         (): any;
@@ -43,41 +43,42 @@ module knockout {
         (): any;
         subscribe(callback: (newValue: any) => void ): koSubscription;
     }
-    export interface koObservable {
+    interface koObservable {
         (value: number): koObservableNumber;
         (value: string): koObservableString;
         (value: bool): koObservableBool;
         (value: any): koObservableAny;
-    };
-    export interface koObservableArray {
-        (array: Array): {
-            (newValue: Array): void;
-            (): Array;
-            subscribe(callback: (newValue: Array) => void ): koSubscription;
+    }
+    interface koObservableArrayBase extends koObservableBase {
+        (newValue: Array): void;
+        (): Array;
+        subscribe(callback: (newValue: Array) => void ): koSubscription;
 
-            pop(): any;
-            push(...items: any[]): void;
-            reverse(): any[];
-            shift(): any;
-            slice(start: number, end?: number): any[];
-            sort(compareFn?: (a, b) => number): any[];
-            splice(start: number): string[];
-            splice(start: number, deleteCount: number, ...items: any[]): any[];
-            unshift(...items: any[]): number;
-            indexOf(item): number;
+        pop(): any;
+        push(...items: any[]): void;
+        reverse(): any[];
+        shift(): any;
+        slice(start: number, end?: number): any[];
+        sort(compareFn?: (a, b) => number): any[];
+        splice(start: number): string[];
+        splice(start: number, deleteCount: number, ...items: any[]): any[];
+        unshift(...items: any[]): number;
+        indexOf(item): number;
 
-            remove(value):any;
-            remove(predicate: (value) =>bool): any;
-            removeAll();
-            removeAll(arrayOfValues: any[]):any[];
+        remove(value): any;
+        remove(predicate: (value) =>bool): any;
+        removeAll();
+        removeAll(arrayOfValues: any[]): any[];
 
-            destroy(value);
-            destroy(predicate: (value) =>bool): any;
-            destroyAll();
-            destroyAll(arrayOfValues: any[]):any[];
-            replace(oldItem, newItem);
-        };
-    };
+        destroy(value);
+        destroy(predicate: (value) =>bool): any;
+        destroyAll();
+        destroyAll(arrayOfValues: any[]): any[];
+        replace(oldItem, newItem);
+    }
+    interface koObservableArray {
+        (array: Array): koObservableArrayBase;
+    }
 
     export function applyBindings(viewModel?, rootNode?: HTMLElement);
     export function toJSON(viewModel, replacer?, space?):string;
@@ -107,6 +108,6 @@ module knockout.utils {
     export function toggleDomNodeCssClass(node:HTMLElement, className:string, shouldHaveClass?:bool);
     export function triggerEvent(element: HTMLElement, eventType: string);
     export function unwrapObservable(value);
-};
+}
 
 declare var ko: knockout;
